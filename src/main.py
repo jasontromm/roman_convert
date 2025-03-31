c1cdd03c4cb48dd1663290c5dceb1d50112eecba
jasontromm <jason@trommetter.org>
2025-03-31 11:55:58 -0400

    Refined error handling in main()

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ def main () :
     roman_numeral = input("Enter a Roman numeral: ")
     try:
         decimal = roman_to_decimal(roman_numeral)
-    except:
-        print (f"Unable to convert {roman_numeral}")
+    except Exception as e:
+        print (e)
         return
     
     print(f"Decimal value of {roman_numeral} is {decimal}")
